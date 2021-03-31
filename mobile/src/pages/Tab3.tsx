aa02c39ef9c8448727fa6e7fa0abddbec3663498
W. Guilherme <ravok123@gmail.com>
2021-03-30 22:01:56 -0300

    [mobile] stable

--- mobile/src/pages/Tab3.tsx
+++ mobile/src/pages/Tab3.tsx
@@ -1,6 +1,38 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab3.css';
+// import './Tab3.css';
+
+// stripe
+import {loadStripe} from '@stripe/stripe-js';
+import {
+  CardElement,
+  Elements,
+  useStripe,
+  useElements,
+} from '@stripe/react-stripe-js';
+
+const CheckoutForm = () => {
+  const stripe:any = useStripe();
+  const elements:any = useElements();
+
+  const handleSubmit = async (event:any) => {
+    event.preventDefault();
+    const {error, paymentMethod} = await stripe.createPaymentMethod({
+      type: 'card',
+      card: elements.getElement(CardElement),
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <CardElement />
+      <button type="submit" disabled={!stripe}>
+          Realizar Pagamento
+      </button>
+    </form>
+  );
+};
+
+const stripePromise = loadStripe('pk_test_51IOUQLFwTfXJuzFuHYQKSj4cfcFQQSHBJkBwBBPsCHJ7CQcnLCEzgjxhIxd7YZvMdCAYY4TlmDz70zBE1v99KV4T00hnAyvvtk');
 
 const Tab3: React.FC = () => {
   return (
@@ -11,12 +43,11 @@ const Tab3: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+
+      <Elements stripe={stripePromise}>
+        <CheckoutForm />
+      </Elements>
+
       </IonContent>
     </IonPage>
   );
